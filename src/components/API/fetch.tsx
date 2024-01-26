d986f2b891c2a8d7e09a942eb02e0ee11a7ca74c
Isaiah Murray <murray.isaiah13@gmail.com>
2024-01-26 16:05:52 -0500

    tested main CRUD endpoints. *exhausted thumbs up*

--- src/components/API/fetch.tsx
+++ src/components/API/fetch.tsx
@@ -1,27 +1,16 @@
-import { useState, useEffect } from "react";
-import { postData } from "./API";
 import { Token } from "../../types";
+
 
 type PropType = {
   setSessionToken: React.Dispatch<React.SetStateAction<Token | null>>;
 };
+
 const Fetch = (props: PropType) => {
-
-  // const [bookArr, setBookArr] = useState([]);
-  const [bodyData, setBodyData] = useState({});
-  const [url, setUrl] = useState("");
-
-  useEffect(() => {
-    const body = { email: "murray.isaiah13@gmail.com", password: "test" };
-    setBodyData(body);
-    setUrl("http://localhost:4000/user/login");
-  }, []);
 
   const handleClick = async (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
     try {
-      const res = await postData(url, bodyData);
-      props.setSessionToken(res?.data.content.token);
+      console.log(props);
     } catch (error) {
       console.error("Error:", error);
     }
