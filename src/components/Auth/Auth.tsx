f6df2add6670eef2604d58e35adebb9d2d3c070d
Isaiah Murray <murray.isaiah13@gmail.com>
2024-01-29 12:51:09 -0500

    Mostyly built out auth component

--- src/components/Auth/Auth.tsx
+++ src/components/Auth/Auth.tsx
@@ -1,53 +1,107 @@
-import React from 'react';
-import { Button, Checkbox, Form, Input } from 'antd';
+import React, { useState } from "react";
+import { Button, Form, Input } from "antd";
+import { postData } from "../API/API";
+import { useProfileContext } from "../Context/ProfileContext";
 
-const onFinish = (values: any) => {
-  console.log('Success:', values);
+const onFinish = (values: unknown) => {
+  //   console.log("Success:", values);
 };
 
-const onFinishFailed = (errorInfo: any) => {
-  console.log('Failed:', errorInfo);
+const onFinishFailed = (errorInfo: unknown) => {
+  //   console.log("Failed:", errorInfo);
 };
 
 type FieldType = {
   username?: string;
-  email?: string,
+  email?: string;
   password?: string;
 };
 
-const Auth: React.FC = () => (
-  <Form
-    name="basic"
-    labelCol={{ span: 8 }}
-    wrapperCol={{ span: 16 }}
-    style={{ maxWidth: 600 }}
-    initialValues={{ remember: true }}
-    onFinish={onFinish}
-    onFinishFailed={onFinishFailed}
-    autoComplete="off"
-  >
-    <Form.Item<FieldType>
-      label="Username"
-      name="username"
-      rules={[{ required: true, message: 'Please input your username!' }]}
-    >
-      <Input />
-    </Form.Item>
+const Auth: React.FC = () => {
+  const { setUser } = useProfileContext();
+  const [login, setLogin] = useState<boolean>(true);
+  const [body, setBody] = useState<FieldType>({});
+  const [username, setUsername] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
 
-    <Form.Item<FieldType>
-      label="Password"
-      name="password"
-      rules={[{ required: true, message: 'Please input your password!' }]}
-    >
-      <Input.Password />
-    </Form.Item>
+  const handleUsernameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(e.target.value);
+  };
 
-    <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-      <Button type="primary" htmlType="submit">
-        Submit
-      </Button>
-    </Form.Item>
-  </Form>
-);
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+  
+  const handleSubmit = async () => {
+    login
+      ? setBody({ email, password })
+      : setBody({ username, email, password });
+    const res = await postData("http://localhost:4000/user/login", body);
+    setUser(res?.data.content.user);
+    // Since setUser is asynchronous, the updated user value might not be available immediately
+    // Use the setUser callback to log the updated user value
+    setUser((updatedUser) => {
+      console.log(updatedUser);
+      return updatedUser;
+    });
+  };
+
+  const usernameField = () => {
+    return login ? (
+      <></>
+    ) : (
+      <Form.Item<FieldType>
+        label="Username"
+        name="username"
+        rules={[{ required: true, message: "Please input your username!" }]}
+      >
+        <Input value={username} onChange={handleUsernameChange} />
+      </Form.Item>
+    );
+  };
+
+  return (
+    <>
+      <Form
+        name="login"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        style={{ maxWidth: 600 }}
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        {usernameField()}
+        <Form.Item<FieldType>
+          label="Email"
+          name="email"
+          rules={[{ required: true, message: "Please input your email!" }]}
+        >
+          <Input value={email} onChange={handleEmailChange} />
+        </Form.Item>
+
+        <Form.Item<FieldType>
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: "Please input your password!" }]}
+        >
+          <Input.Password value={password} onChange={handlePasswordChange} />
+        </Form.Item>
+
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Button type="primary" htmlType="submit" onClick={handleSubmit}>
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </>
+  );
+};
 
 export default Auth;
