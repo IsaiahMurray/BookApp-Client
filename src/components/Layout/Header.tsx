25e4199a11b9ba2349b8f6e5d154aae567784925
Isaiah Murray <murray.isaiah13@gmail.com>
2024-01-29 10:20:56 -0500

    Created Header/Nav file

---
+++ src/components/Layout/Header.tsx
@@ -0,0 +1,25 @@
+import { Layout, Menu } from "antd";
+
+const { Header } = Layout;
+
+const items = new Array(15).fill(null).map((_, index) => ({
+  key: index + 1,
+  label: `nav ${index + 1}`,
+}));
+
+const Navbar = () => {
+  return (
+    <Header style={{ display: "flex", alignItems: "center" }}>
+      <div className="demo-logo" />
+      <Menu
+        theme="dark"
+        mode="horizontal"
+        defaultSelectedKeys={["2"]}
+        items={items}
+        style={{ flex: 1, minWidth: 0 }}
+      />
+    </Header>
+  );
+};
+
+export default Navbar;
